81a883c295df56f6df86ba823d0f2e43c117b44c
Santiago Libonati <santiago.libonati@gmail.com>
2024-11-27 12:49:49 -0300

    Arreglados errores en eval1

--- src/Eval1.hs
+++ src/Eval1.hs
@@ -60,12 +60,13 @@ stepComm :: MonadState m => Comm -> m Comm
 stepComm Skip = return Skip
 stepComm (Let v exp) = do x <- evalExp exp
                           update v x
+                          return Skip
 stepComm (Seq Skip c2) = stepComm c2 
 stepComm (Seq c1 c2) = do x <- stepComm c1
                           stepComm (Seq x c2)
 stepComm (IfThenElse exp c1 c2) = do b <- evalExp exp
                                      if b then stepComm c1 else stepComm c2
-stepComm (Repeat exp) c = stepComm (Seq c (IfThenElse exp (Repeat exp c) Skip))
+stepComm (Repeat exp c) = stepComm (Seq c (IfThenElse exp (Repeat exp c) Skip))
                         
 
 -- Evalua una expresion
@@ -85,7 +86,7 @@ evalExp (Times e1 e2) = do x <- evalExp e1
                            return (x * y)
 evalExp (Div e1 e2) = do x <- evalExp e1
                          y <- evalExp e2
-                         return (x / y)
+                         return (x `div` y)
 evalExp BTrue = return True
 evalExp BFalse = return False
 evalExp (Lt e1 e2) = do x <- evalExp e1
@@ -101,7 +102,7 @@ evalExp (Or e1 e2) = do x <- evalExp e1
                         y <- evalExp e2
                         return (x || y)
 evalExp (Not e) = do x <- evalExp e
-                     return (not e)                   
+                     return (not x)                   
 evalExp (Eq e1 e2) = do x <- evalExp e1
                         y <- evalExp e2
                         return (x == y)
