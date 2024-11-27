924e00b11d59ce07a52e63e9c16677afcca31d84
TomasMaiza <tomimaiza@gmail.com>
2024-11-27 17:49:11 -0300

    gooool (arreglado eval2 falta arreglar pp)

--- src/aux.hs
+++ src/aux.hs
@@ -1,71 +1,3 @@
-module Eval2
-  ( eval
-  , Env
-  )
-where
-
-import           AST
-import           Monads
-import qualified Data.Map.Strict               as M
-import           Data.Maybe
-import           Prelude                 hiding ( fst
-                                                , snd
-                                                )
-import           Data.Strict.Tuple
-import           Control.Monad                  ( liftM
-                                                , ap
-                                                )
-
--- Entornos
-type Env = M.Map Variable Int
-
--- Entorno nulo
-initEnv :: Env
-initEnv = M.empty
-
--- Mónada estado, con manejo de errores
-newtype StateError a =
-  StateError { runStateError :: Env -> Either Error ( Pair a Env) }
-
-
--- Para calmar al GHC
-instance Functor StateError where
-  fmap = liftM
-
-instance Applicative StateError where
-  pure  = return
-  (<*>) = ap
-
--- Ejercicio 2.a: Dar una instancia de Monad para StateError:
-instance Monad StateError where
-  return x = StateError (\env -> return (x :!: env))
-  m >>= f = StateError (\env -> do (x :!: env') <- runStateError m env
-                                   runStateError (f x) env')
-
--- Ejercicio 2.b: Dar una instancia de MonadError para StateError:
-instance MonadError StateError where
-  throw e = StateError (\_ -> Left e)
-
--- Ejercicio 2.c: Dar una instancia de MonadState para StateError:
-instance MonadState StateError where
-  lookfor v = StateError (\env -> lookfor' v env)
-    where lookfor' :: Variable -> Env -> Either Error (Pair Int Env)
-          lookfor' v s = case M.lookup v s of
-                            Nothing -> Left UndefVar
-                            Just x -> Right (x :!: s)
-  update v i = StateError (\env -> return (() :!: M.insert v i env))
-
--- Ejercicio 2.d: Implementar el evaluador utilizando la monada StateError.
--- Evalua un programa en el estado nulo
-eval :: Comm -> Either Error Env
-eval c = do (() :!: env) <- runStateError (stepCommStar c) initEnv
-            return env
-
--- Evalua multiples pasos de un comando, hasta alcanzar un Skip
-stepCommStar :: (MonadState m, MonadError m) => Comm -> m ()
-stepCommStar Skip = return ()
-stepCommStar c    = stepComm c >>= \c' -> stepCommStar c'
-
 -- Evalua un paso de un comando
 stepComm :: (MonadState m, MonadError m) => Comm -> m Comm
 stepComm Skip = return Skip
@@ -75,7 +7,7 @@ stepComm (Let v exp) = do x <- evalExp exp
 stepComm (Seq Skip c2) = stepComm c2 
 stepComm (Seq c1 c2) = do x <- stepComm c1
                           stepComm (Seq x c2)
-stepComm (IfThenElse exp c1 c2) = do b <- evalExp exp
+stepComm (IfThenElse exp c1 c2) = do (b :!: env) <- evalExp exp
                                      if b then stepComm c1 else stepComm c2
 stepComm (Repeat exp c) = stepComm (Seq c (IfThenElse exp (Repeat exp c) Skip))
 
@@ -101,11 +33,11 @@ evalExp (Div e1 e2) = do x <- evalExp e1
                           0 -> throw DivByZero
                           _ -> return (x `div` y)
 evalExp (VarDec v) = do x <- lookfor v
-                        x' <- return (x - 1)
+                        (x' :!: env') <- return (x - 1)
                         update v x'
                         return x'
 evalExp (VarInc v) = do x <- lookfor v
-                        x' <- return (x + 1)
+                        (x' :!: env') <- return (x + 1)
                         update v x'
                         return x'
 evalExp BTrue = return True
@@ -130,5 +62,3 @@ evalExp (Eq e1 e2) = do x <- evalExp e1
 evalExp (NEq e1 e2) = do x <- evalExp e1
                          y <- evalExp e2
                          return (x /= y)
-
-
